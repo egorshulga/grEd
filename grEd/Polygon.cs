63537521d41f81f37dd572f1d0eb75a60c5d9dcf
Egor Shulga <scrilan@gmail.com>
2015-03-19 23:33:53 +0300

    polygon implemented

---
+++ grEd/Polygon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace grEd
+{
+	class Polygon : Polyline
+	{
+		public Polygon(Panel panel) : base(panel)
+		{
+			IsFilled = true;
+			IsClosed = true;
+			FillRule = FillRule.Nonzero;
+		}
+
+//		public Polygon(Panel panel, IEnumerable<Point> points) : base(panel, points)
+//		{
+//			IsFilled = true;
+//		}
+
+
+	}
+}
