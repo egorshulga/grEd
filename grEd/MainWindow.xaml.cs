70365d4589dee156ee559c5b055bc52169342ab7
Egor Shulga <scrilan@gmail.com>
2015-04-03 19:42:03 +0300

    preparings for the 2nd lab

--- grEd/MainWindow.xaml.cs
+++ grEd/MainWindow.xaml.cs
@@ -9,60 +9,13 @@ namespace grEd
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		readonly FiguresList figuresList;
 		public MainWindow()
 		{
 			InitializeComponent();
 			figuresList = new FiguresList(Canvas);
 		}
 
-		private void Draw_Click(object sender, RoutedEventArgs e)
-		{
-			drawFigures();
-		}
-
-		FiguresList figuresList;
-		void drawFigures()
-		{
-			var line = new Line( new Point(10, 10), new Point(400, 350)) { stroke = Brushes.Aqua };
-			var ellipse = new Ellipse( new Point(55, 10), new Point(195, 100)) { fill = Brushes.Tan };
-			var curve = new Curve( new Point(5, 110), new Point(50, 90), new Point(150, 170), new Point(300, 120))
-			{
-				strokeThickness = 10,
-				stroke = Brushes.Crimson
-			};
-			var polygone = new Polygone( 
-				new List<Point>
-				{
-					new Point(2, 200),
-					new Point(5, 205),
-					new Point(150, 180),
-					new Point(395, 220),
-					new Point(400, 215)
-				}) { fill = Brushes.Blue };
-			var triangle = new Triangle(new Point(207, 3), new Point(254, 22), new Point(220, 200));
-			var rightTriangle = new RightTriangle(new Point(270, 10), new Point(350, 197))
-			{
-				fill = Brushes.Chartreuse,
-				stroke = null,
-			};
-			var rectangle = new Rectangle(new Point(375, 20), new Point(500, 307))
-			{
-				fill = Brushes.Coral
-			};
-
-			figuresList.Add(line);
-			figuresList.Add(ellipse);
-			figuresList.Add(curve);
-			figuresList.Add(polygone);
-			figuresList.Add(triangle);
-			figuresList.Add(rightTriangle);
-			figuresList.Add(rectangle);
-		}
-
-		private void Remove_Click(object sender, RoutedEventArgs e)
-		{
-			figuresList.Dispose();
-		}
 
 	}
 }
