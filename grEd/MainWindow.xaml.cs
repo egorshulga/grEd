af8566a469d790dcb9c7abaf64e1c1673b29d207
Egor Shulga <scrilan@gmail.com>
2015-04-03 20:57:39 +0300

    trying to implement color (and other) bindings

--- grEd/MainWindow.xaml.cs
+++ grEd/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace grEd
@@ -13,8 +14,36 @@ namespace grEd
 		public MainWindow()
 		{
 			InitializeComponent();
+
 			figuresList = new FiguresList(Canvas);
+
+			comboBoxesInitialization();
 		}
+
+		private void comboBoxesInitialization()
+		{
+			strokeColorBoxInitialization();
+		}
+
+
+
+		public List<SolidColorBrush> colorsList { get; set; }
+		public SolidColorBrush selectedStrokeColor { get; set; }
+		public SolidColorBrush selectedFillColor { get; set; }
+		public FillRule selectedFillRule { get; set; }
+		public int selectedStrokeThickness { get; set; }
+
+		private void strokeColorBoxInitialization()
+		{
+
+		}
+
+		private void Close_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+
 
 
 	}
