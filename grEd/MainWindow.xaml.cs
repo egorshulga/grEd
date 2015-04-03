4752e6f627435e07129a5bec1aec0d9981392820
Egor Shulga <scrilan@gmail.com>
2015-04-03 21:41:21 +0300

    comboboxes are filled properly
    TODO: implement normal color names

--- grEd/MainWindow.xaml.cs
+++ grEd/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
@@ -20,31 +22,67 @@ namespace grEd
 			comboBoxesInitialization();
 		}
 
+
 		private void comboBoxesInitialization()
 		{
-			strokeColorBoxInitialization();
+			colorsListInitialization();
+			thicknessesListInitialization();
+			fillRulesInitialization();
+			DataContext = this;
+
+			selectedFillColor = selectedStrokeColor = Brushes.Black;
+			selectedFillRule = FillRule.Nonzero;
+			selectedStrokeThickness = 1;
 		}
 
-
-
 		public List<SolidColorBrush> colorsList { get; set; }
+		public List<int> thicknessesList { get; set; }
+		public List<FillRule> fillRules { get; set; }
+
 		public SolidColorBrush selectedStrokeColor { get; set; }
 		public SolidColorBrush selectedFillColor { get; set; }
 		public FillRule selectedFillRule { get; set; }
 		public int selectedStrokeThickness { get; set; }
 
-		private void strokeColorBoxInitialization()
+		private void colorsListInitialization()
 		{
+			colorsList = new List<SolidColorBrush>();
 
+			Type brushesType = typeof(Brushes);
+			// Get all static properties
+			var properties = brushesType.GetProperties(BindingFlags.Static | BindingFlags.Public);
+			foreach (var prop in properties)
+			{
+				string name = prop.Name;
+				SolidColorBrush brush = (SolidColorBrush)prop.GetValue(null, null);
+				colorsList.Add(brush);
+			}
 		}
+		private void thicknessesListInitialization()
+		{
+			thicknessesList = new List<int>();
+			for (int i = 1; i < 10; i++)
+				thicknessesList.Add(i);
+		}
+		private void fillRulesInitialization()
+		{
+			fillRules = new List<FillRule> {FillRule.EvenOdd, FillRule.Nonzero};
+		}
+
+
+
+
+
+
+
+
+
+
+
 
 		private void Close_Click(object sender, RoutedEventArgs e)
 		{
 			Application.Current.Shutdown();
 		}
-
-
-
-
 	}
 }
