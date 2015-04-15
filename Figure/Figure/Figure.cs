f4e0933b021bef46e79b7f49582bf595fb229c51
Egor Shulga <scrilan@gmail.com>
2015-04-15 21:29:58 +0300

    Figure.dll implemented
    NOTE: dynamic load WAS NOT implemented recently.

---
+++ Figure/Figure/Figure.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Figure
+{
+	public class Figure
+	{
+		private readonly Path path = new Path();
+		private readonly PathGeometry pathGeometry = new PathGeometry();
+		private readonly PathFigure pathFigure = new PathFigure();
+
+		protected Point startPoint { get { return pathFigure.StartPoint; } set { pathFigure.StartPoint = value; isStartPointSet = true; } }
+		protected bool isStartPointSet { get; set; }
+		protected Brush stroke { get { return path.Stroke; } set { path.Stroke = value; } }
+		protected double strokeThickness { get { return path.StrokeThickness; } set { path.StrokeThickness = value; } }
+		protected Brush fill { get { return path.Fill; } set { path.Fill = value; } }
+		protected FillRule fillRule { get { return pathGeometry.FillRule; } set { pathGeometry.FillRule = value; } }
+		protected bool isClosed { get { return pathFigure.IsClosed; } set { pathFigure.IsClosed = value; } }
+		protected bool isFilled { get { return pathFigure.IsFilled; } set { pathFigure.IsFilled = value; } }
+
+
+		private const bool isStroked = true;
+		private readonly Brush defaultBrush = Brushes.Black;
+		private const double rotationAngle = 0;
+		private const bool isLargeArc = true;
+
+
+		protected Figure()
+		{
+			pathGeometry.Figures.Add(pathFigure);
+			path.Data = pathGeometry;
+
+			stroke = defaultBrush;
+			isFilled = true;
+			isClosed = true;
+			fillRule = FillRule.Nonzero;
+		}
+
+		public void drawItOn(Panel panel)
+		{
+			panel.Children.Add(path);
+		}
+
+		public void removeFrom(Panel panel)
+		{
+			panel.Children.Remove(path);
+		}
+
+		protected BezierSegment addBezierSegment(Point point1, Point point2, Point endPoint)
+		{
+			BezierSegment segment = new BezierSegment(point1, point2, endPoint, isStroked);
+			addSegment(segment);
+			return segment;
+		}
+
+		protected LineSegment addLineSegment(Point endPoint)
+		{
+			LineSegment segment = new LineSegment(endPoint, isStroked);
+			addSegment(segment);
+			return segment;
+		}
+
+		protected ArcSegment addArcSegment(Point endPoint, Size semiAxes, SweepDirection sweepDirection = SweepDirection.Counterclockwise)
+		{
+			ArcSegment segment = new ArcSegment(endPoint, semiAxes, rotationAngle, isLargeArc, sweepDirection, isStroked);
+			addSegment(segment);
+			return segment;
+		}
+
+		private void addSegment(PathSegment segment)
+		{
+			pathFigure.Segments.Add(segment);
+		}
+
+	}
+
+}
---
+++ Figure/Figure/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Figure
+{
+	interface IDrawable
+	{
+		void mouseDrawHandler(Point point);
+		void mousePreviewHandler(Point point);
+		bool isFigureFinished();
+		void stopDrawing();
+	}
+}
